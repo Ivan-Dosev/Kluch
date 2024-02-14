2afc3a6902060034e3ded7546488446c8a8a4d17
Ivan Dimitrov <biobeehive@abv.bg>
2024-02-14 16:40:19 +0200

    Initial Commit

---
+++ Kluch/KluchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KluchApp.swift
+//  Kluch
+//
+//  Created by Dosi Dimitrov on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct KluchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
